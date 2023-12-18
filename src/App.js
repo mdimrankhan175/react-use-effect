d5fafab71d469df570e6058466147a7c258f1cbb
MOHAMMAD IMRAN KHAN <124260776+mdimrankhan175@users.noreply.github.com>
2023-12-18 09:05:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+  const [data, setData]=useState([])
+  const [isLoading, setIsLoading]=useState(true)
+
+useEffect(
+  ()=>{
+    const fetchData=async ()=>{
+      try{
+        const response=await fetch('https://apis.ccbp.in/blogs')
+        const result= await response.json()
+        setData(result)
+        setIsLoading(false)
+      }catch(error){
+        console.error('Error fetching data:',error)
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+    return()=>{}
+  },[]
+)
+
+return(
+  <div className='data-fetcher-container'>
+    <h1>Data Fetcher</h1>
+    {isLoading?(<p>Loading...</p>)
+    :(
+      <ul className="blog-list">
+          {data.map(item => (
+            <li key={item.id} className="blog-item">
+              <img
+                src={item.image_url}
+                alt={item.title}
+                className="blog-image"
+              />
+              <div className="blog-item-info">
+                <p className="blog-item-topic">{item.topic}</p>
+                <h1 className="blog-item-title">{item.title}</h1>
+                <div className="author-info">
+                  <img
+                    src={item.avatar_url}
+                    height="100px"
+                    width="100px"
+                    alt={item.id}
+                    className="avatar"
+                  />
+                  <p className="author-name">{item.author}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+    )
+    }
+  </div>
+)
+
 }
 
-export default App;
+export default App
